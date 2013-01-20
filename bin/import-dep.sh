5a162130b6e32cff0ad92186a5dd67fe61a942d2
sletuffe <git@letuffe.org>
2013-01-20 19:08:55 +0100

    faire que la configuration des chemins de dossier soit un maximum centralisée

--- bin/import-dep.sh
+++ bin/import-dep.sh
@@ -2,20 +2,21 @@
 # ©Cléo 2010
 # GPL v3 or higher — http://www.gnu.org/licenses/gpl-3.0.html
 
-cd /data/project/cadastre.openstreetmap.fr/data
+. /config
+cd $data_dir
+
+Qadastre2OSM="$bin_dir/Qadastre2OSM"
 
 [ -d $1 ] || mkdir $1
 cd $1
-./data/project/cadastre.openstreetmap.fr//bin/Qadastre2OSM --list $1 > $1-liste.txt
+$Qadastre2OSM --list $1 > $1-liste.txt
 sed -i "s/\([^ ]*\) - \(.*\) (.*)/\1 \2/" $1-liste.txt
 while read l 
 	do
 		code=`echo $l|cut -d' ' -f1`
 		ville=`echo $l|sed "s/[^ ]* //"`
-		echo /data/project/cadastre.openstreetmap.fr//bin/Qadastre2OSM --download $1 $code "$ville"
-		/data/project/cadastre.openstreetmap.fr//bin/Qadastre2OSM --download $1 $code "$ville"
-		echo /data/project/cadastre.openstreetmap.fr//bin/Qadastre2OSM --convert $code "$ville"
-		/data/project/cadastre.openstreetmap.fr//bin/Qadastre2OSM --convert $code "$ville"
+		$Qadastre2OSM --download $1 $code "$ville"
+		$Qadastre2OSM --convert $code "$ville"
 		rm -f "$code-$ville.tar.bz2"
 		tar cvf "$code-$ville.tar" --exclude="*.pdf" $code-$ville*
 		bzip2 -f "$code-$ville.tar"
