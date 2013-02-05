7dd8bc7693e9bab269394663fe24c52db8725918
windu2b <windu.2b@gmail.com>
2013-02-05 20:34:03 +0100

    mise en forme du code HTML
    Certaines balises étaient mal imbriquées, et le bouton 'submit' n'avait pas de message en français (plus clair ainsi).

--- web/index.php
+++ web/index.php
@@ -61,22 +61,24 @@ else {
 	</fieldset>
 	<fieldset id="fville">
 		<legend>Choix de la ville</legend>
-		<select id="ville" name="ville">
 		<img src="images/throbber_16.gif" style="display:none;" alt="pending" id="throbber_ville" />
-	</select>
-	<br /><p style="font-size:small;"><img src="images/info.png" alt="!" style="vertical-align:sub;" />&nbsp;Le code de la ville est son <a href="http://fr.wikipedia.org/wiki/Code_Insee#Identification_des_collectivit.C3.A9s_locales_.28et_autres_donn.C3.A9es_g.C3.A9ographiques.29">code INSEE</a>, pas son code postal</p>
+		<select id="ville" name="ville">
+		</select>
+		<br />
+		<p style="font-size:small;"><img src="images/info.png" alt="!" style="vertical-align:sub;" />&nbsp;Le code de la ville est son <a href="http://fr.wikipedia.org/wiki/Code_Insee#Identification_des_collectivit.C3.A9s_locales_.28et_autres_donn.C3.A9es_g.C3.A9ographiques.29">code INSEE</a>, pas son code postal</p>
 	</fieldset>
 	<fieldset id="mise_en_garde">
 		<legend>Mise en garde</legend>
-	<br /><p>
+		<br />
+		<p>
 		L'intégration de données "batiments" en provenance du cadastre n'est pas triviale, si vous ne venez pas de <a href="http://wiki.openstreetmap.org/wiki/WikiProject_France/Cadastre/Import_semi-automatique_des_b%C3%A2timents">la page page suivante</a>, il est vivement recommandé d'aller la lire !
-		</p
+		</p>
 		<p>
 		Pour les limites de communes, c'est pas trivial non plus et la <a href="http://wiki.openstreetmap.org/wiki/WikiProject_France/Limites_administratives/Tracer_les_limites_administratives">documentation est ici.</a>
-	</p>
+		</p>
 	</fieldset>
 	<div>
-		<input type="submit" />
+		<input type="submit" value="Générer" />
 	</div>
 </form>
 <p>
