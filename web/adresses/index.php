fbfa0066d1335e0d95496b06f63586e730ba9e06
tyndare <tyndare@wanadoo.fr>
2014-02-22 16:25:45 +0100

    Tentative de fusion de l'interface web bati et adresses.
    La page web adresses/index.php peut dorénavant générer
    soit l'export du bati soit celui des adresses.
    
    La page web inclue aussi désormais un champ recherche
    pour filtrer la liste de départements et de villes.

--- web/adresses/index.php
+++ web/adresses/index.php
@@ -6,16 +6,140 @@ if( isset( $_POST['dep'] ) )
 	$dep = $_POST['dep'];
 if( isset( $_POST['ville'] ) )
 	$ville = $_POST['ville'];
+if( isset( $_POST['type'] ) )
+	$type = $_POST['type'];
 $command = "";
 ?>
+<style>
+#dep {
+    width: 200px;
+}
+#ville {
+    width: 300px;
+}
+#recherche_dep {
+    color: grey;
+}
+#recherche_ville {
+    color: grey;
+}
+</style>
+<script type="text/javascript">
+var previous_depCode = '';
+function getDepartement( ville )
+{
+        depIndex = document.getElementById( "dep" ).selectedIndex;
+        depCode = document.getElementById( "dep" ).options[depIndex].value;
+		if (depCode != previous_depCode) {
+		  previous_depCode = depCode;
+		  params = "dep=" + depCode;
+		  //document.getElementById("throbber_ville").style.display = "inline";
+		  xhr = new XMLHttpRequest();
+		  xhr.onreadystatechange = handler;
+		  xhr.open("POST", "getDepartement.php?ville=" + ville, true );
+		  xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
+		  xhr.setRequestHeader( "Content-length", params.length );
+		  xhr.setRequestHeader( "Connection", "close" );
+		  xhr.send(params);
+		}
+}
+function handler()
+{
+        if( this.readyState == 4 && this.status == 200 )
+        {
+                document.getElementById( "ville" ).innerHTML = this.responseText;
+                ville_filter = new SelectBoxFilter(ville);
+				document.getElementById( "recherche_ville" ).value = '';
+				filter_ville();
+        //      document.getElementById("throbber_ville").style.display = "none";
+        }
+}
+function normalize(text) {
+  text = text.toLowerCase();
+  text = text.replace(/[-_']/g,' ');
+  text = text.replace(/[aàÀ]/g,'a');
+  text = text.replace(/[éèêëÉÈÊË]/g,'e');
+  text = text.replace(/[ïîÏÎ]/g,'i');
+  text = text.replace(/[ôÔ]/g,'o');
+  text = text.replace(/[ùÙûÛ]/g,'u');
+  text = text.replace(/\bsaint\b/g,'st');
+  return text;
+}
+function SelectBoxFilter(selectbox) {
+    this.selectbox = selectbox;
+    if (typeof selectboxfilter_backgroundcolor == 'undefined') {
+        selectboxfilter_backgroundcolor = selectbox.style.backgroundColor;
+    }
+    this.optionscopy = new Array();
+    for(var i=0; i<selectbox.options.length; i++) {
+        var option = selectbox.options[i];
+        this.optionscopy[i] = new Option(option.text, option.value);
+    }
+    this.filter = function(reg_exp) {
+        var selectedValue = this.selectbox.value;
+        var selectedIndex = 0;
+        this.selectbox.options.length = 0;
+        var nb = 0;
+        var nbvalue = 0;
+        for(var i=0; i<this.optionscopy.length; i++) {
+          option = this.optionscopy[i];
+          if((option.value == "") || (normalize(option.text).search(reg_exp) != -1)) {
+              if (option.value != "") nbvalue++;
+              if (option.value == selectedValue) selectedIndex = nb;
+              this.selectbox.options[nb++] = new Option(option.text, option.value, false);
+          }
+        }
+        this.selectbox.selectedIndex = selectedIndex;
+        if (nbvalue == 0) {
+          this.selectbox.style.backgroundColor = "#d22";
+        } else if (nbvalue == 1) {
+          this.selectbox.style.backgroundColor = "#bf6";
+        } else {
+          this.selectbox.style.backgroundColor = selectboxfilter_backgroundcolor;
+        }
+    }
+}
+
+function filter_dep() {
+  var text = document.getElementById("recherche_dep").value
+  if (typeof previous_filter_dep_text == 'undefined') {
+      previous_filter_dep_text = '';
+  }
+  if ((text != "Recherche") && (text != previous_filter_dep_text)) {
+    previous_filter_dep_text = text;
+    var dep = document.getElementById("dep");
+    if (typeof dep_filter == 'undefined') {
+      dep_filter = new SelectBoxFilter(dep);
+    }
+    // recherche en début de mot (ou avec un 0 pour les code de département):
+    dep_filter.filter(new RegExp("\\b0?" + normalize(text)));
+    if (dep.options.length == 2) {
+      dep.selectedIndex = 1;
+      getDepartement();
+    }
+  }
+}
+function filter_ville() {
+  var text = document.getElementById("recherche_ville").value
+  if (text != "Recherche") {
+    var ville = document.getElementById("ville");
+    if (typeof ville_filter == 'undefined') {
+      ville_filter = new SelectBoxFilter(ville);
+    }
+    // recherche en début de mot:
+    ville_filter.filter(new RegExp("\\b" + normalize(text)));
+  }
+}
+</script>
+
 <div id="conditions-utilisation">
 <p>
 Ce service et les données du cadastre disponibles ici sont exclusivement réservés à l'usage des contributeurs OpenStreetMap. <a href="http://wiki.openstreetmap.org/wiki/Cadastre_Fran%C3%A7ais/Conditions_d%27utilisation">En savoir plus</a>
 </p>
 </div>
-<div id='information'> 
+<div id='information'>
 <?php
-if( isset( $dep ) && isset( $ville ) )
+if( isset( $dep ) && isset( $ville ) && isset( $type ))
 {
 	if( !file_exists( $locks_path . '/' . $dep ) )
 	{
@@ -26,30 +150,40 @@ if( isset( $dep ) && isset( $ville ) )
 	{
 		@mkdir( $logs_path );
 		mkdir( $logs_path . '/' . $dep );
-        }
-	$lock_file = $locks_path . '/' . $dep . '/' . $dep . '-' . $ville . '-adresses.lock';
+	}
+	$log_file = $logs_path . '/' . $dep . '/' . $dep . '-' . $ville . '-' . $type . '.log';
+	$lock_file = $locks_path . '/' . $dep . '/' . $dep . '-' . $ville . '-' . $type . '.lock';
 	if( file_exists( $lock_file ) && ((time() - filemtime ( $lock_file )) < 60*60)) {
 		echo 'Import en cours';
-        }
+	}
 	else
 	{
 		if( touch( $lock_file ) )
 		{
-		        chmod( $lock_file, 0664);
+		    chmod( $lock_file, 0664);
 			if ($do_we_log)
 			{
-				$log = fopen( $logs_path . '/log-adresses.txt', 'a+' );
-				fwrite( $log, date( 'd-m-Y H:i:s' ) . ' ' . $_SERVER['REMOTE_ADDR'] . ' : ' . $dep . ' ' . $ville . ";\n" );
+				$log = fopen( $logs_path . '/log.txt', 'a+' );
+				fwrite( $log, date( 'd-m-Y H:i:s' ) . ' ' . $_SERVER['REMOTE_ADDR'] . ' : ' . $dep . ' ' . $ville . "" . $type . ";\n" );
 				fclose( $log );
-				$import_adresse_logs="2>&1 |tee \"$logs_path/%s/%s-%s-adresses.log\" 2>&1";
+				if ($type == "adresses") { 
+					$log_cmd="2>&1 |tee \"$log_file\"";
+				} else {
+					$log_cmd="> \"$log_file\" 2>&1";
+				}
 			}
 			else
-				$import_adresses_logs="";
+				$log_cmd="";
 			$v = explode( '-', $ville, 2 );
-			$command = sprintf( "cd %s && ./import-adresses.sh %s %s \"%s\" $import_adresses_logs", $bin_path, $dep, $v[0], trim( $v[1] ), $dep, $dep, $ville );
-			//exec( $command );
-			//echo 'Import ok. Acc&egrave;s <a href="data/' . $dep . '">aux fichiers</a> - <a href="data/' . $dep . '/' . $v[0] . '-' . trim( $v[1] ) . '.tar.bz2">&agrave; l\'archive</a>';
-			//unlink( $lock_file );
+			if ($type == "adresses") { 
+				$command = sprintf( "cd %s && ./import-adresses.sh %s %s \"%s\" $log_cmd", $bin_path, $dep, $v[0], trim( $v[1] ));
+			} else {
+			    $command = sprintf( "cd %s && ./import-ville.sh %s %s \"%s\" $log_cmd", $bin_path, $dep, $v[0], trim( $v[1] ));
+				exec( $command );
+				echo 'Import ok. Acc&egrave;s <a href="data/' . $dep . '">aux fichiers</a> - <a href="data/' . $dep . '/' . $v[0] . '-' . trim( $v[1] ) . '.tar.bz2">&agrave; l\'archive</a>';
+				unlink( $lock_file );
+				$command = '';
+			}
 		}
 		else
 			echo 'Something went wrong';
@@ -82,21 +216,36 @@ else
 	echo 'No data';
 ?>
 		</select>
+		<input value="Recherche" type="text" id="recherche_dep" name="recherche_dep" maxlength="40" size="20" onfocus="javascript:if(this.value == 'Recherche') this.value='';" onchange="javascript:filter_dep();" onkeyup="javascript:filter_dep();" onpaste="javascript:filter_dep();" onmouseup="javascript:filter_dep();"/>
 	</fieldset>
 	<fieldset id='fville'>
 		<legend>Choix de la commune</legend>
 		<img src='images/throbber_16.gif' style='display:none;' alt='pending' id='throbber_ville' />
 		<select id='ville' name='ville'>
 		</select>
+		<input value="Recherche" type="text" id="recherche_ville" name="recherche_ville" maxlength="60" size="20" onfocus="javascript:if(this.value == 'Recherche') this.value='';" onchange="javascript:filter_ville();" onkeyup="javascript:filter_ville();" onpaste="javascript:filter_ville();" onmouseup="javascript:filter_ville();"/>
+
 		<br />
 		<p style='font-size:small;'><img src='images/info.png' alt='!' style='vertical-align:sub;' />&nbsp;Le code indiqué à coté du nom de la commune est son <a href='http://fr.wikipedia.org/wiki/Code_Insee#Identification_des_collectivit.C3.A9s_locales_.28et_autres_donn.C3.A9es_g.C3.A9ographiques.29'>code INSEE</a>, pas son code postal</p>
 		<p style='font-size:small;'>Seules les communes existant au format vecteur au cadastre sont listées</p>
 	</fieldset>
+	<fieldset id='ftype'>
+		<legend>Choix du type de données</legend>
+<?php
+$bati_checked = ((isset($type)) && ($type=="bati")) ? "checked" : "";
+$adresses_checked = ((!isset($type)) || ($type=="adresses")) ? "checked" : "";
+?>
+		<input type="radio" name="type" value="bati" <?php echo $bati_checked;?>>Bâti &amp; Limites</input><br/>
+		<input type="radio" name="type" value="adresses" <?php echo $adresses_checked;?>>Adresses</input><br/>
+	</fieldset>
 	<fieldset id='mise_en_garde'>
 		<legend>Mise en garde</legend>
-		<br />
 		<p>
-		L'intégration de données 'adresses' en provenance du cadastre n'est pas triviale, si vous ne venez pas de <a href='http://wiki.openstreetmap.org/wiki/WikiProject_France/Cadastre/Import_semi-automatique_des_adresses'>la page suivante</a>, il est vivement recommandé d'aller la lire !
+		L'intégration de données <i><u>bâtiments</u></i> en provenance du cadastre n'est pas triviale, si vous ne venez pas de <a href='http://wiki.openstreetmap.org/wiki/WikiProject_France/Cadastre/Import_semi-automatique_des_b%C3%A2timents'>la page suivante</a>, il est vivement recommandé d'aller la lire !
+		<br/>
+		Pour les <i><u>limites</u></i> de communes, ce n'est pas trivial non plus et la <a href='http://wiki.openstreetmap.org/wiki/WikiProject_France/Limites_administratives/Tracer_les_limites_administratives'>documentation est ici.</a>
+		<br/>
+		Pour l'intégration des données <i><u>adresses</u></i>, <a href='http://wiki.openstreetmap.org/wiki/WikiProject_France/Cadastre/Import_semi-automatique_des_adresses'>il faut lire cette page</a>.
 		</p>
 	</fieldset>
 	<div>
@@ -107,7 +256,7 @@ else
 Note: Vous pensez avoir trouvé un bug ? <a href='http://trac.openstreetmap.fr/newticket?component=export%20cadastre&owner=vdct'>Vous pouvez le signaler ici (composant export cadastre)</a>
 </p>
 <?php
-if( $_POST['ville'] )
+if( isset($_POST['ville']) && $_POST['ville'])
 {
 ?>
 <script type='text/javascript'>
