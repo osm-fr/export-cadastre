1eea20c0148c09329d1067eba8738a7b74daa24d
vdct <git@letuffe.org>
2014-03-16 13:45:54 +0100

    bascule sur overpass car oapi-fr en panne

--- bin/cadastre-housenumber/cherche_fantoir_et_osm_highways.py
+++ bin/cadastre-housenumber/cherche_fantoir_et_osm_highways.py
@@ -146,7 +146,9 @@ def open_osm_overpass(requete, cache_filename, metropole=False):
             overvass_server = "http://oapi-fr.openstreetmap.fr/oapi/interpreter?"
         else:
             overvass_server = "http://overpass-api.de/api/interpreter?"
-        url = overvass_server + urllib.urlencode({'data':requete})
+# 16/03/14 : patch temporaire pour ignorer oapi-fr car osm3 en panne
+        overvass_server = "http://overpass-api.de/api/interpreter?"
+	url = overvass_server + urllib.urlencode({'data':requete})
         sys.stdout.write((urllib.unquote(url) + "\n").encode("utf-8"))
         sys.stdout.flush()
         write_stream_to_file(urllib2.urlopen(url), cache_filename)
