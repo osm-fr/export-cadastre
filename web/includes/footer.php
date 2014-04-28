a1a88ad39a7fa0c6d6476ecd6adca6b32bbab2a4
tyndare <tyndare@wanadoo.fr>
2014-04-28 21:46:40 +0200

    Ajout image cartouche osm-fr

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -1,7 +1,7 @@
 </div>
 <div id="footer">
 	<div style="position:fixed; bottom:5px; right:5px;"><a href='http://www.april.org'><img src='http://www.chtinux.org/images/png/cartouche_april-jadhere.png' alt='Promouvoir et soutenir le logiciel libre' style="border: none;"/></a></div>
-	<div style="position:fixed; bottom:5px; right:90px;"><a href='http://www.openstreetmap.fr'><img src='http://www.cleo-carto.com/images/cartouche_osm-fr.png' alt='OpenStreetMap France' style="border: none;"/></a></div>
+	<div style="position:fixed; bottom:5px; right:90px;"><a href='http://www.openstreetmap.fr'><img src='images/cartouche_osm-fr.png' alt='OpenStreetMap France' style="border: none;"/></a></div>
 </div>
 </body>
 </html>
