3554d954407c875f9e7b6dcfecc5834d40d8aaaa
tyndare <tyndare@wanadoo.fr>
2015-02-04 21:00:38 +0100

    Adresses: recherche des mots accentué: filtre l'apostrophe.

--- bin/cadastre-housenumber/cherche_fantoir_et_osm_highways.py
+++ bin/cadastre-housenumber/cherche_fantoir_et_osm_highways.py
@@ -277,8 +277,8 @@ def get_dict_accents_mots(osm_noms):
         # On essaye de parser l'ensemble des noms extraits du cadastre pour
         # en faire un dictionaire de remplacement a appliquer
         for node in osm_noms.nodes.itervalues():
-          if ('name' in node.tags) and not ('place' in node.tags): # on évite les nœuds place=* qui sont écrit en majuscule sans accents
-            for mot in node.tags['name'].replace("_"," ").replace("-"," ").split():
+          if ('name' in node.tags): #and not ('place' in node.tags): # on évite les nœuds place=* qui sont écrit en majuscule sans accents
+            for mot in node.tags['name'].replace("_"," ").replace("-"," ").replace("'"," ").split():
                 if len(mot) > 1:
                     mot_norm = to_ascii(mot).upper()
                     if mot_norm != mot.upper(): # il contient des accents
