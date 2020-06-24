35cb59bb6dd85d8866e6308d22459950d14ac220
marc-marc-marc <marc_marc_irc@hotmail.com>
2020-06-24 09:27:22 +0200

    suppression du lien trac plus utile. la page contient deja github

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -1,7 +1,6 @@
 </div>
 <div id="footer">
     <a href="https://github.com/osm-fr/export-cadastre" title="Voir le code source sur GitHub"><img src="images/git.png"></a>&nbsp;
-    <a href="https://trac.openstreetmap.fr/newticket?component=export%20cadastre&amp;owner=vdct&amp;cc=tyndare" title="Signaler un Bug"><img src="images/trac.png"></a>&nbsp;
     <span style="flex-grow:1;"> </span>
     <a href='http://www.openstreetmap.fr'><img src='images/cartouche_osm-fr.png' alt='OpenStreetMap France' style="border: none;"/></a>&nbsp;
 <div>
